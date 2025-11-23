d4dee4c1c8f58a841622571616f35a0d21f613b8
NinoGalea <nino.galea.fr@gmail.com>
2025-11-23 23:16:29 +0100

    Derniers changement

--- assets/scripts/pizzasDropdown.js
+++ assets/scripts/pizzasDropdown.js
@@ -6,10 +6,12 @@ function pizzaDropdownToogle() {
     switch (pizzasList.attributes['data-state'].value) {
         case "closed":
             pizzasList.attributes['data-state'].value = "opened";
+            pizzasButton.innerHTML = "Voir moins";
             break;
-            
+
         case "opened":
             pizzasList.attributes['data-state'].value = "closed";
+            pizzasButton.innerHTML = "Voir toutes nos pizzas";
             break;
 
         default:
